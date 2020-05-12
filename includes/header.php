d894ac1437ee7de9d15410833cc6e6c9e9bd7330
root <root@localhost.localdomain>
2020-05-12 01:35:23 +0100

    bug testing

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,7 @@
 <tr>
 		<td align="center" height="100" WIDTH=500 > <a href="./index.php?page=home.php"><Img src="./IMG/PaginaInicial.png"></a></td>
 		<td align="center" height="100" WIDTH=500 > <a href="./index.php?page=Filmes.php"><Img src="./IMG/Filmes.png"></a></td>
-		<td align="center" height="100" WIDTH=500 > <a href="./index.php?page=Serie.php"><Img src="./IMG/Serie.png"></a></td>
+		<td align="center" height="100" WIDTH=500 > <a href="./index.php?page=Seri.php"><Img src="./IMG/Serie.png"></a></td>
 		<td align="center" height="100" WIDTH=500 > <a href="./index.php?page=Animes.php"><Img src="./IMG/Animes.png"></a></td>
 		<td align="center" height="100" WIDTH=500 > <a href="./index.php?page=Ajuda.php"><Img src="./IMG/Ajuda.png"></a></td>
 	</tr>
