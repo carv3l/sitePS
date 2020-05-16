46da745150255ed96ac554863deb1a0ce3e10cf7
unknown <carv3l@vivaldi.net>
2020-05-16 19:29:41 +0100

    debuging

--- index.php
+++ index.php
@@ -101,6 +101,11 @@
 		);
 
 		for ($i = 0; $i <= 4; $i++) {
+
+
+
+			echo '<script type="text/javascript">alert("'.$array[$i].'");</script>';
+
 			if (strstr($path, $array[$i]) != NULL){
 				$alerta = "Path Traversal Detectado, a string foi: "+$array[$i];
 				echo '<script type="text/javascript">alert("'.$alerta.'");</script>';
